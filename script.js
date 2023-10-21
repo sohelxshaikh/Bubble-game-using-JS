1ea0ad2069f127615eba65f9221cd9843077f2a1
Sohail Shaikh <sohailhacker7786@gmail.com>
2023-10-21 10:45:51 +0530

    functions added

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-timer = 60;
+var timer = 60;
+var score = 0;
+var hitnum= 0;
+
+
+function increaseScore(){
+    score = score+ 10;
+    document.querySelector("#scoreval").textContent= score;
+    
+}
 
 function createBubble(){
     var clutter = "";
 
 for(var i = 1 ; i <=154 ; i++ ){
-    var rn = Math.floor(Math.random()*10);
-    clutter += `<div class="bubble">${rn}</div>`;
+    hitnum = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${hitnum}</div>`;
 }
 document.querySelector("#pbtm").innerHTML = clutter;
 }
@@ -29,6 +38,16 @@ function getNewHit(){
     var rn = Math.floor(Math.random()*10)
     document.querySelector("#hitval").textContent = rn;
 }
+
+document.querySelector("#pbtm").addEventListener("click",function(dets){
+    var clickednum = Number(dets.target.textContent);
+   
+    if( hitnum === clickednum ){
+        increaseScore();
+        createBubble();
+        getNewHit();
+    };
+});
 
 getNewHit();
 setTimer();
